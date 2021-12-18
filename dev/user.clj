ed9b7aecb5cab7188a7484bbec77c72156d58eae
Nazarii Bardiuk <nazarii@bardiuk.com>
2021-12-18 20:51:05 +0000

    feat: setup reflection warnings

--- dev/user.clj
+++ dev/user.clj
@@ -2,3 +2,5 @@
   (:require
    [clojure.tools.namespace.repl :refer [refresh refresh-all]]))
 
+(set! *warn-on-reflection* true)
+(set! *unchecked-math* :warn-on-boxed)
--- src/p_1_0_01.clj
+++ src/p_1_0_01.clj
@@ -3,10 +3,11 @@
             [clojure2d.color :as color]))
 
 (defn draw [canvas window ^long _frameno _state]
-  (let [[mouse-x mouse-y] (c2d/mouse-pos window)]
+  (let [mouse-x (c2d/mouse-x window)
+        mouse-y (c2d/mouse-y window)]
     (doto canvas
-      (c2d/set-background (color/from-HSB* [(/ mouse-y 2.) 255. 255.]))
-      (c2d/set-color (color/from-HSB* [(- 360 (/ mouse-y 2.)) 255. 255.]))
+      (c2d/set-background (color/from-HSB* [(* (/ 255. 360.) mouse-y 0.5) 255. 255.]))
+      (c2d/set-color (color/from-HSB* [(- 360 (* (/ 255. 360.) mouse-y 0.5)) 255. 255.]))
       (c2d/crect 360 360 (inc mouse-x) (inc mouse-x)))))
 
 (comment
